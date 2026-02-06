278116b545344e1fdb82c1932b3a1ff5a42fb013
Michał Sitarski <45768956+michalsitarski@users.noreply.github.com>
2026-02-06 11:38:21 +0100

    Update script.js

--- script.js
+++ script.js
@@ -54,16 +54,16 @@
 })();
 */
 const messages = [
-    "Are you sure?",
-    "Really sure??",
-    "Are you positive?",
-    "Pookie please...",
-    "Just think about it!",
-    "If you say no, I will be really sad...",
-    "I will be very sad...",
-    "I will be very very very sad...",
-    "Ok fine, I will stop asking...",
-    "Just kidding, say yes please! ❤️"
+    "Jesteś pewna?",
+    "Zobacz jak się Puma ładnie uśmiecha, no weż!",
+    "nie bądź taka",
+    "Może jednak zmienisz zdanie?",
+    "Spójrz jeszcze raz na Pumcie",
+    "Ona by ci pwoieidziała żebyś się zgodziła",
+    "Proszę Kochanie",
+    "Zostań moją Walentynką!",
+    "Bedzie fajnie",
+    "Obiecuję ❤️"
 ];
 
 let messageIndex = 0;
